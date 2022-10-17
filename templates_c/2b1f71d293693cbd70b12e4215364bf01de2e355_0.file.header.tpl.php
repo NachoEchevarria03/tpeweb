02ac706d4fdeac6cfd3287ac1cb0b1e04232660c
Juan Ignacio Echevarria <nachoecheva4@gmail.com>
2022-10-17 14:21:10 -0300

    agregue css

--- templates_c/2b1f71d293693cbd70b12e4215364bf01de2e355_0.file.header.tpl.php
+++ templates_c/2b1f71d293693cbd70b12e4215364bf01de2e355_0.file.header.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.2.1, created on 2022-10-17 19:02:23
+/* Smarty version 4.2.1, created on 2022-10-17 19:14:49
   from 'C:\xampp\htdocs\web2\tpeweb\templates\header.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.2.1',
-  'unifunc' => 'content_634d8a9f5806f9_32006141',
+  'unifunc' => 'content_634d8d8912b815_63715676',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '2b1f71d293693cbd70b12e4215364bf01de2e355' => 
     array (
       0 => 'C:\\xampp\\htdocs\\web2\\tpeweb\\templates\\header.tpl',
-      1 => 1666026141,
+      1 => 1666026843,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_634d8a9f5806f9_32006141 (Smarty_Internal_Template $_smarty_tpl) {
+function content_634d8d8912b815_63715676 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -31,7 +31,7 @@ function content_634d8a9f5806f9_32006141 (Smarty_Internal_Template $_smarty_tpl)
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="./css/styles.css">
     <title>ÑAGUCHI</title>
 </head>
 <body>
